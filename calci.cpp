42f11e842290d01c136b90b72580dff3af1fefa1
supriyabade <supriya.bade@cumminscollege.in>
2014-08-05 09:31:13 +0530

    Update calci.cpp
    This program add two no.

--- calci.cpp
+++ calci.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
+#include<stdio.h>
 using namespace std;
 int main()
 {
 cout<<"Good Morning";
+
+void add()
+{
+  int a=10;
+  int b=20;
+  int c;
+  c=a+b;
+  cout<<"c="<<c;
+  
+}
 }
